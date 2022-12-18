512565eeaf7a91d32f48384140faa6647854c3c9
Tyler OHearn <tylerdavidohearn@gmail.com>
2022-12-17 21:15:44 -0500

    - v2 error fixed from v1
    - need to add function to Finish button on completion screen.

--- app/src/main/java/com/tylersapps/quizard/QuizQuestionsActivity.kt
+++ app/src/main/java/com/tylersapps/quizard/QuizQuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.tylersapps.quizard
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -14,24 +15,27 @@ import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mCurrentPosition: Int = 1
-    private var mQuestionsList: ArrayList<Question>? = null
-    private var mSelectedOptionPosition: Int = 0
-
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
     private var tvQuestion: TextView? = null
     private var ivImage: ImageView? = null
-
     private var tvOptionOne: TextView? = null
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
     private var btnSubmit: Button? = null
 
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mCorrectAnswers: Int = 0
+    private var mUserName: String? = null
+    private var mSelectedOptionPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions2)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         progressBar = findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progress)
@@ -42,6 +46,9 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
         btnSubmit = findViewById(R.id.btn_submit)
+        mQuestionsList = Constants.getQuestions()
+
+        setQuestion()
 
         tvOptionOne?.setOnClickListener(this)
         tvOptionTwo?.setOnClickListener(this)
@@ -51,32 +58,34 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
         mQuestionsList = Constants.getQuestions()
 
-        setQuestion()
-
-
     }
 
     private fun setQuestion() {
 
-        mCurrentPosition = 1
-        val question: Question = mQuestionsList!![mCurrentPosition - 1]
-        ivImage?.setImageResource(question.image)
-        progressBar?.progress = mCurrentPosition
-        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
-        tvQuestion?.text = question.question
-        tvOptionOne?.text = question.optionOne
-        tvOptionTwo?.text = question.optionTwo
-        tvOptionThree?.text = question.optionThree
-        tvOptionFour?.text = question.optionFour
-
+        val question: Question =
+            mQuestionsList!![mCurrentPosition - 1] // Getting the question from the list with the help of current position.
+        defaultOptionsView()
         if (mCurrentPosition == mQuestionsList!!.size) {
             btnSubmit?.text = "FINISH"
         } else {
             btnSubmit?.text = "SUBMIT"
         }
+        progressBar?.progress =
+            mCurrentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text =
+            "$mCurrentPosition" + "/" + progressBar?.max // Setting up the progress text
+
+        // Now set the current question and the options in the UI
+        tvQuestion?.text = question.question
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
 
     private fun defaultOptionsView() {
+
         val options = ArrayList<TextView>()
         tvOptionOne?.let {
             options.add(0, it)
@@ -101,21 +110,9 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
-        defaultOptionsView()
-
-        mSelectedOptionPosition = selectedOptionNum
-
-        tv.setTextColor(Color.parseColor("#363A43"))
-        tv.setTypeface(tv.typeface, Typeface.BOLD)
-        tv.background = ContextCompat.getDrawable(
-            this,
-            R.drawable.selected_option_border_bg
-        )
-    }
-
     override fun onClick(view: View?) {
         when (view?.id) {
+
             R.id.tv_option_one -> {
                 tvOptionOne?.let {
                     selectedOptionView(it, 1)
@@ -142,19 +139,36 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btn_submit ->{
+
                 if (mSelectedOptionPosition == 0){
+
                     mCurrentPosition++
 
                     when{
                         mCurrentPosition <= mQuestionsList!!.size ->{
+
                             setQuestion()
+                        }
+                        else -> {
+
+                            val intent =
+                                Intent(this@QuizQuestionsActivity, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionsList?.size)
+                            startActivity(intent)
+                            finish()
                         }
                     }
                 }else {
                     val question = mQuestionsList?.get(mCurrentPosition - 1)
+
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.incorrect_option_border_bg)
+                    }else{
+                        mCorrectAnswers++
                     }
+
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
 
                     if(mCurrentPosition == mQuestionsList!!.size){
@@ -162,36 +176,51 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                     }else{
                         btnSubmit?.text = "GO TO NEXT QUESTION"
                     }
-                    
+
                     mSelectedOptionPosition = 0
                 }
             }
         }
     }
 
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
+        defaultOptionsView()
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(
+            Color.parseColor("#363A43")
+        )
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this@QuizQuestionsActivity,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
     private fun answerView(answer: Int, drawableView: Int) {
+
         when (answer) {
+
             1 -> {
                 tvOptionOne?.background = ContextCompat.getDrawable(
                     this,
                     drawableView
                 )
             }
-
             2 -> {
                 tvOptionTwo?.background = ContextCompat.getDrawable(
                     this,
                     drawableView
                 )
             }
-
             3 -> {
                 tvOptionThree?.background = ContextCompat.getDrawable(
                     this,
                     drawableView
                 )
             }
-
             4 -> {
                 tvOptionFour?.background = ContextCompat.getDrawable(
                     this,
