4eea36e914a1a2a32fead090304aa7209da3cb5b
Tyler OHearn <tylerdavidohearn@gmail.com>
2022-12-17 20:15:37 -0500

    - v1 of Quizard, first 2 activities completed.
    - Error on "SUBMIT" no progress to next screen.

---
+++ app/src/main/java/com/tylersapps/quizard/Constants.kt
@@ -0,0 +1,90 @@
+package com.tylersapps.quizard
+
+object Constants {
+
+    fun getQuestions():ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Australia", "Armenia", "Austria",
+            1
+        )
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Argentina", "Australia", "Armenia", "Austria",
+            2
+        )
+        questionsList.add(que2)
+
+        val que3 = Question(
+            3, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Belgium", "Bulgaria", "Bolivia", "Peru",
+            1
+        )
+        questionsList.add(que3)
+
+        val que4 = Question(
+            4, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Bolivia", "Bulgaria", "Barbados", "Brazil",
+            4
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Botswana", "Colombia", "Denmark", "Austria",
+            3
+        )
+        questionsList.add(que5)
+
+        val que6 = Question(
+            6, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "El Salvador", "Fiji", "Zimbabwe", "Mexico",
+            2
+        )
+        questionsList.add(que6)
+
+        val que7 = Question(
+            7, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Mozambique", "Singapore", "Germany", "Spain",
+            3
+        )
+        questionsList.add(que7)
+
+        val que8 = Question(
+            8, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Germany", "Mexico", "South Africa", "India",
+            4
+        )
+        questionsList.add(que8)
+
+        val que9 = Question(
+            9, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait", "Uruguay", "Laos", "Japan",
+            1
+        )
+        questionsList.add(que9)
+
+        val que10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia", "New Zealand", "Austria", "Sweden",
+            2
+        )
+        questionsList.add(que10)
+
+        return questionsList
+    }
+}
---
+++ app/src/main/java/com/tylersapps/quizard/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tylersapps.quizard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val etName : EditText = findViewById(R.id.etName)
+        btnStart.setOnClickListener {
+
+            if(etName.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
